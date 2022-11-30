8d8a7d7a7e00f8153f7a7b323fc09fc3d86f722c
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-30 10:15:30 +0530

    Initialized the Repo

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-gray-900 text-white mx-auto text-center">Index Page</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ pages/zenpage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-gray-900 text-white">ZenPage</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
